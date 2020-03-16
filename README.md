35b0bd3f2765ea8f4dc74d51503d39c6063137c2
Sayantan Ghosh <gsayantan01@gmail.com>
2020-03-16 21:59:28 +0530

    Initial COmmit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Ecommerce Website and Android app
---
+++ backend/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  //TODO : Incomplete
+  password: {
+    type: String,
+    trim: true
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0
+  },
+  purchases: {
+    type: Array,
+    default: []
+  }
+});
